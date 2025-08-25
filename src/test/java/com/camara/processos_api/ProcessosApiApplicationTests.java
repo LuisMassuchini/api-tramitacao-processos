613cb0144678543970d01d6620cee2a752ea48de
Luís Massuchini <luis.massuchini@gmail.com>
2025-08-25 10:32:09 -0300

    fix: teste

--- src/test/java/com/camara/processos_api/ProcessosApiApplicationTests.java
+++ src/test/java/com/camara/processos_api/ProcessosApiApplicationTests.java
@@ -2,12 +2,15 @@ package com.camara.processos_api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@ActiveProfiles("test")
 class ProcessosApiApplicationTests {
 
 	@Test
 	void contextLoads() {
+		// contexto deve subir usando H2
 	}
 
 }
